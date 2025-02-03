044808f4d248b3e8a1fac273ce7c548a02c564c3
Cristian Mcknight <cristianmcknight@Cristians-MacBook-Air.local>
2025-02-03 15:19:47 -0300

    updates

--- pagesScripts.js
+++ pagesScripts.js
@@ -25,8 +25,31 @@ goToCoversPages.forEach((element) =>
 const logo = document.querySelector(".logo");
 logo.addEventListener("click", () => (window.location.href = logo.dataset.url));
 
-// if (window.innerWidth <= 450) {
-//   const helloTxtTop = document.querySelector(".txtNImgTop");
-//   helloTxtTop.innerHTML = "";
+/*Display menu in mobile */
+const menuContainer = document.querySelector(".menuContainer");
+if (window.innerWidth <= 450) {
+  const html2 = `
+  <div class="menu">
+       <div data-url="bodyCoverings.html"><h1>BODY COVERS</h1></div>
+       <div data-url="ourHistory.html"><h1>OUR HISTORY</h1></div>
+       <div data-url="cart.html"><h1>CART</h1></div>
+  </div>`;
+  menuContainer.insertAdjacentHTML("afterbegin", html2);
+}
 
-// }
+/*display the menu */
+const menuBtn = document.querySelector(
+  ".header div:nth-child(2) img:nth-child(2)"
+);
+const menu = document.querySelector(".menu");
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("active"); // Toggle 'active' class
+});
+
+/*go to the respective page when one of .menu divs is clicked */
+const menuDivs = document.querySelectorAll(".menu div");
+menuDivs.forEach((div) => {
+  div.addEventListener("click", () => {
+    window.location.href = div.dataset.url;
+  });
+});
